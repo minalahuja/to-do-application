d78d1d3490ca1979da774641a9a9468d5ba34954
Minal Ahuja <minal.ahuja@freshworks.com>
2022-06-06 14:43:51 +0530

    Changes as per review comments

--- app/serializers/completed.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.RESTSerializer.extend({
-    normalizeResponse(store,primaryModelClass, payload, id, requestType){
-        payload = {completed : payload}
-        console.log(payload);
-        return this._super(store,primaryModelClass, payload, id, requestType);
-    }
-});
